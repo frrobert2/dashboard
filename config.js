e25a6561306ebb4aa2f5335d83f5c97a4f460f1d
Rev. Fr. Robert Bower <39407737+frrobert2@users.noreply.github.com>
2025-04-01 18:04:49 -0400

    Initial Commit

--- config.js
+++ config.js
@@ -1,109 +1,116 @@
-const disableSetup = false;
-var topBarCenterText = `VA3HDL - FN04ga`;
+// CUT START
+var disableSetup = false; // Manually set to true to disable setup page menu option
+var topBarCenterText = "Glory to Jesus Christ!!! Glory to Him Forever!!!";
 
-// Grid layout
+// Grid layout desired
 var layout_cols = 4;
-var layout_rows = 3;
+var layout_rows = 1;
 
 // Menu items
-// Structure is as follows HTML Color code, Option, target URL, scaling 1=Original Size, side (optional, nothing is Left, "R" is Right)
+// Structure is as follows: HTML Color code, Option, target URL, scaling 1=Original Size, side (optional, nothing is Left, "R" is Right)
 // The values are [color code, menu text, target link, scale factor, side],
 // add new lines following the structure for extra menu options. The comma at the end is important!
 var aURL = [
-  ["2196F3", "CLUBLOG", "https://clublog.org/livestream/VA3HDL", "1.7"],
   [
-    "2196F3",
-    "CONTEST",
-    "https://www.contestcalendar.com/fivewkcal.html",
-    "1",
+    "#8ff0a4",
+    "Calendar",
+    "https://www.stnectarios.org/EasternRiteCalendar/",
+    1,
+    "L"
   ],
-  ["2196F3", "DX CLUSTER", "https://dxcluster.ha8tks.hu/map/", "1"],
   [
-    "2196F3",
-    "LIGHTNING",
-    "https://map.blitzortung.org/#3.87/36.5/-89.41",
-    "1",
-    "R",
+    "#8ff0a4",
+    "The Prologue from Ochrid Site 1",
+    "https://www.ochrid.org/index.en",
+    1,
+    "L"
   ],
-  ["2196F3", "PISTAR", "http://pi-star.local/", "1.2"],
   [
-    "2196F3",
-    "RADAR",
-    "https://weather.gc.ca/?layers=alert,radar&center=43.39961001,-78.53212031&zoom=6&alertTableFilterProv=ON",
-    "1",
+    "#8ff0a4",
+    "The Prologue from Ochrid Site 2",
+    "https://www.ohrid-prolog.com/?lang=en",
+    1,
+    "L"
+  ],
+  [
+    "#2196f3",
+    "ROCOR",
+    "https://synod.com/synod/indexeng.htm",
+    1,
     "R"
   ],
-  ["2196F3", "TIME.IS", "https://time.is/", "1", "R"],
   [
-    "2196F3",
-    "WEATHER",
-    "https://openweathermap.org/weathermap?basemap=map&cities=true&layer=temperature&lat=44.0157&lon=-79.4591&zoom=5",
-    "1",
-    "R",
+    "#2196f3",
+    "Diocese of Chicago and Mid-America",
+    "https://chicagodiocese.org/",
+    1,
+    "R"
   ],
   [
-    "2196F3",
-    "WINDS",
-    "https://earth.nullschool.net/#current/wind/surface/level/orthographic=-78.79,44.09,3000",
-    "1",
-    "R",
+    "#3584e4",
+    "Saint Paul the Apostle Orthodox Monastery",
+    "https://www.saintpaulsmonastery.org/",
+    1,
+    "R"
   ],
+  [
+    "#3584e4",
+    "Holy Trinity Monastery",
+    "https://jordanville.org/",
+    1,
+    "R"
+  ],
+  [
+    "#8ff0a4",
+    "Jordanville Prayerbook - 1986",
+    "https://www.holynewmartyrs.org/prayerbook",
+    1,
+    "L"
+  ],
+  [
+    "#8ff0a4",
+    "Orthodox Liturgical Texts  And Resources",
+    "http://www.saintjonah.org/services/",
+    1,
+    "L"
+  ]
 ];
 
-// Dashboard items
-// Structure is Title, Image Source URL
-// [Title, Image Source URL],
+// Feed items
+// Structure is as follows: target URL
+// The values are [target link]
+var aRSS = [];
+
+// Dashboard Tiles items
+// Tile Structure is Title, Source URL
+// To display a website on the tiles use "iframe|" keyword before the tile URL
+// [Title, Source URL],
 // the comma at the end is important!
-// You can't add more items because there are only 12 placeholders on the dashboard
-// but you can replace the titles and the images with anything you want.
 var aIMG = [
-  ["RADAR", "https://radar.weather.gov/ridge/standard/CONUS_loop.gif"],
   [
-    "LOCAL RADAR",
-    "https://radar.weather.gov/ridge/standard/KNQA_loop.gif",
+    "",
+    "S21lg.webp"
   ],
   [
-    "NOAA D-RAP",
-    "https://services.swpc.noaa.gov/images/animations/d-rap/global/d-rap/latest.png",
+    "",
+    "T22lg.webp"
   ],
   [
-    "ISS POSITION",
-    "https://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544",
+    "",
+    "J23lg.webp"
   ],
   [
-    "SATELLITE CAN",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/can/EXTENT3/GOES16-CAN-EXTENT3-1125x560.gif",
-  ],
-  [
-    "SATELLITE CGL",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/cgl/EXTENT3/GOES16-CGL-EXTENT3-600x600.gif",
-  ],
-  [
-    "LIGHTNING",
-    "https://images.lightningmaps.org/blitzortung/america/index.php?animation=usa",
-  ],
-  [
-    "LIGHTNING LOCAL",
-    "https://www.blitzortung.org/en/Images/image_b_ny.png",
-  ],
-  ["YOUTUBE EXAMPLE", "iframe|https://www.youtube.com/embed/fzPFaXAV_2Y?autoplay=1&mute=1"],
-  [
-    "WEBSITE EXAMPLE",
-    "iframe|https://globe.adsbexchange.com/?airport=YYZ",
-  ],
-  ["VIDEO EXAMPLE", "https://himawari8.nict.go.jp/movie/720/20240611_pifd.mp4"],
-  ["HF PROPAGATION",
-    "https://www.hamqsl.com/solar101vhf.php"],
+    "",
+    "CS956lg.webp"
+  ]
 ];
 
-// Image rotation intervals in milliseconds per tile - If the line below is commented, all tiles will be rotated every 30000 milliseconds (30s)
+// Image rotation intervals in milliseconds per tile - If the line below is commented, tiles will be rotated every 5000 milliseconds (5s)
 var tileDelay = [
-  11200,10000,11000,10100,
-  10200,10500,10300,10600,
-  30400,60700,60900,10800
+  0,
+  0,
+  0,
+  0
 ];
 
-var aRSS = [
-  ["https://www.amsat.org/feed/"],          // Example RSS feed
-  ["https://daily.hamweekly.com/atom.xml"], // Example Atom feed
-  ];
+// CUT END
